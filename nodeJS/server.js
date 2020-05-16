97fd6675fa1c4e8db20374c261a004fb75309cee
endifly <ducth_miw@hotmail.com>
2020-05-16 22:36:53 +0700

    update express

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -5,6 +5,11 @@ const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => res.send('Hello World'))
 
+app.get('/getMenu',(req, res)=> {
+    console.log(req.data)
+    res.send("I got that")
+})
+
 app.listen(PORT, () => {
 
     console.log(`Server is running on port : ${PORT}`)
