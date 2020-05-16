fafa2a5a5af32b7ea73badb635dd0e073d4e1908
Tana Chanchiew <terktana18@gmail.com>
2020-05-16 14:53:21 +0700

    add material ui

--- front/asr-web/src/App.js
+++ front/asr-web/src/App.js
@@ -1,7 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+// material ui
+import {
+  Button,
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+} from "@material-ui/core";
 
-class App extends Component {
+import MenuIcon from '@material-ui/icons/Menu';
+
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -9,10 +19,26 @@ class App extends Component {
     };
   }
 
+  renderHeader = () => {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start"  color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+
   render() {
     return (
       <div>
-
+        {this.renderHeader()}
       </div>
     );
   }
